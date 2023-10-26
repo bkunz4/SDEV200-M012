fb29357aab96aa434ba4a15ae23e6e647ec5e4d8
bkunz4 <23880812-bkunz4@users.noreply.replit.com>
2023-10-26 18:50:28 +0000

    complete

--- creditcard.java
+++ creditcard.java
@@ -1,30 +1,29 @@
 import java.util.Scanner;
 
 public class creditcard {
-	/** Main Method */
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in); // Create a Scanner
+		/**Create a Scanner*/
+    Scanner input = new Scanner(System.in); 
 
-		// Prompt the user to enter a credit card number as a long integer
+		/**Asks the user to enter a credit card number */
 		System.out.print("Enter a credit card number as a long integer: ");
 		long number = input.nextLong();
-
+    /**Prints the results*/
 		System.out.println(
 			number + " is " + (isValid(number) ? "valid" : "invalid"));
 	}
 
-	/** Return true if the card number is valid */
+	/**Return true if the card number is valid */
 	public static boolean isValid(long number) {
 		boolean valid =
 			(getSize(number) >= 13 && getSize(number) <= 16) &&
 			(prefixMatched(number, 4) || prefixMatched(number, 5) ||
 			prefixMatched(number, 37) || prefixMatched(number, 6)) &&
 			((sumOfDoubleEvenPlace(number) + sumOfOddPlace(number)) % 10 == 0);
-
 		return valid;
 	}
 	
-	/** Get the result from Step 2 */
+	/**Get the result from Step 2 */
 	public static int sumOfDoubleEvenPlace(long number) {
 		int sum = 0;
 		String num = number + "";
@@ -35,7 +34,7 @@ public class creditcard {
 	}
 	
 	/** Return this number if it is a single digit, otherwise,
-	* return the sum of the two digits */
+   * return the sum of the two digits */
 	public static int getDigit(int number) {
 		if (number < 9)
 			return number;
@@ -43,7 +42,7 @@ public class creditcard {
 			return number / 10 + number % 10;
 	}
 	
-	/** Return sum of odd-place digits in number */
+	/**Return sum of odd-place digits in number */
 	public static int sumOfOddPlace(long number) {
 		int sum = 0;
 		String num = number + "";
@@ -53,19 +52,19 @@ public class creditcard {
 		return sum;
 	}
 	
-	/** Return true if the digit d is a prefix for number */
+	/**Return true if the digit d is a prefix for number */
 	public static boolean prefixMatched(long number, int d) {
 		return getPrefix(number, getSize(d)) == d;
 	}
 	
-	/** Return the number of digits in d */
+	/**Return the number of digits in d */
 	public static int getSize(long d) {
 		String num = d + "";
 		return num.length();
 	}
 	
 	/** Return the first k number of digits from number. If the
-	* number of digits in number is less than k, return number. */
+   * number of digits in number is less than k, return number. */
 	public static long getPrefix(long number, int k) {
 		if (getSize(number) > k)  {
 			String num = number + "";
